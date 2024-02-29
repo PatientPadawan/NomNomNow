8e7c49b0aaa0b7bbe04c25ce817335e8f64a1d7d
PatientPadawan <49569154+PatientPadawan@users.noreply.github.com>
2024-02-29 12:40:53 -0700

    typecast

--- src/app/_components/ordersTable.tsx
+++ src/app/_components/ordersTable.tsx
@@ -26,26 +26,30 @@ const OrdersTable = () => {
   return (
     <tbody>
       {data.length > 0
-        ? data.map((order) => (
-            <tr
-              className={`${order.status !== "Delivered" && "bg-red-50"} text-sm md:text-base`}
-              key={order.id}
-            >
-              <td className="hidden px-1 py-6 md:table-cell md:text-center">
-                {order.id}
-              </td>
-              <td className="px-1 py-6">{order.createdAt.toDateString()}</td>
-              <td className="px-1 py-6">
-                {parseFloat(order.price).toFixed(2)}
-              </td>
-              <td className="hidden px-1 py-6 md:table-cell">
-                {order.products.map(
-                  (item) => `${item.title}(${item.quantity}), `,
-                )}
-              </td>
-              <td className="px-1 py-6">{order.status}</td>
-            </tr>
-          ))
+        ? data.map((order) => {
+            // converting Decimal to string
+            const price = order.price as unknown as string;
+            return (
+              <tr
+                className={`${order.status !== "Delivered" && "bg-red-50"} text-sm md:text-base`}
+                key={order.id}
+              >
+                <td className="hidden px-1 py-6 md:table-cell md:text-center">
+                  {order.id}
+                </td>
+                <td className="px-1 py-6">{order.createdAt.toDateString()}</td>
+                <td className="px-1 py-6">
+                  {parseFloat(price).toFixed(2)}
+                </td>
+                <td className="hidden px-1 py-6 md:table-cell">
+                  {order.products.map(
+                    (item) => `${item.title}(${item.quantity}), `,
+                  )}
+                </td>
+                <td className="px-1 py-6">{order.status}</td>
+              </tr>
+            );
+          })
         : "No order history"}
     </tbody>
   );
